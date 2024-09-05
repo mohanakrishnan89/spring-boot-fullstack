f267e28b55da69a54568b5c66d9d2af57bb90e65
Mohan <mohanakrishnan89@gmail.com>
2024-09-05 20:49:08 +0530

    Restored

--- backend/src/main/java/com/amigoscode/Main.java
+++ backend/src/main/java/com/amigoscode/Main.java
@@ -9,6 +9,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 
 @SpringBootApplication
@@ -16,7 +19,6 @@ public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
-
     }
 
 @Bean
