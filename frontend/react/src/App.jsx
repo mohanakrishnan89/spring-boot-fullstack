563a7a480a868283622a0bd131d35f8be0b66ece
Mohan <mohanakrishnan89@gmail.com>
2024-09-25 11:49:37 +0530

    Commiting Frontend files and CORS changes

---
+++ frontend/react/src/App.jsx
@@ -0,0 +1,56 @@
+import {Wrap,
+        WrapItem,
+        Spinner,
+        Text} from "@chakra-ui/react";
+import SidebarWithHeader from "./components/shared/SideBar.jsx"
+import CardWithImage from "./components/Card.jsx";
+import {useEffect,useState} from "react";
+import {getCustomer} from "./services/client.js";
+
+const App = () =>{
+    const[isLoading, setIsLoading] = useState(false);
+    const[customers,setCustomers] = useState([]);
+
+    useEffect(() => {
+        setIsLoading(true);
+        getCustomer().then(res =>{
+            setCustomers(res.data);
+        }).catch(err =>{
+            console.log(err)
+        }).finally(() =>{
+            setIsLoading(false);
+        })
+    }, []);
+
+    if(isLoading)
+        return (
+            <SidebarWithHeader>
+                <Spinner
+                    thickness='4px'
+                    speed='0.65s'
+                    emptyColor='gray.200'
+                    color='blue.500'
+                    size='xl'
+                />
+            </SidebarWithHeader>
+        )
+    if(customers.length <= 0)
+        return (
+            <SidebarWithHeader>
+               <Text>No Customers found</Text>
+            </SidebarWithHeader>
+        )
+    return(
+        <SidebarWithHeader>
+            <Wrap justify={"center"} spacing={"30px"}>
+            {customers.map((customer,index) =>(
+                <WrapItem key={index}>
+                    <CardWithImage {... customer}/>
+                </WrapItem>
+            ))}
+            </Wrap>
+        </SidebarWithHeader>
+    )
+}
+
+export default App
---
+++ frontend/react/src/UserProfile.jsx
@@ -0,0 +1,16 @@
+const UserProfile = ({name,age,gender,imageNumber, ...props}) =>{
+
+    gender = gender === "MALE" ? "men" : "women";
+    return (
+        <div>
+            <p>{name}</p>
+            <p>{age}</p>
+            <img
+                src={`https://randomuser.me/api/portraits/${gender}/${imageNumber}.jpg`}/>
+            {props.children}
+        </div>
+    )
+}
+
+export default UserProfile
+
